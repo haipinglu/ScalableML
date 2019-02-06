81e7832f3fd4bf3e9dd89e86618ad14d91cf7848
haipinglu <haipinglu@users.noreply.github.com>
2019-02-06 22:59:05 +0000

    Update

--- HPC/Lab1_SubmitBatch.sh
+++ HPC/Lab1_SubmitBatch.sh
@@ -2,8 +2,8 @@
 #$ -l h_rt=2:00:00  #time needed
 #$ -pe smp 2 #number of cores
 #$ -l rmem=4G #number of memery
-#$ -o COM6012_Lab1_YourName #This is where your output and errors are logged.
-#$ -j y # normal and error outputs into a single file
+#$ -o COM6012_Lab1.output #This is where your output and errors are logged.
+#$ -j y # normal and error outputs into a single file (the file above)
 #$ -M youremail@shef.ac.uk #Notify you by email, remove this line if you don't like
 #$ -m ea #Email you when it finished or aborted
 #$ -cwd # Run job from current directory
