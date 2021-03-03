cee58d21559cbf5a342ee006361fee348837da1c
Haiping Lu <haipinglu@users.noreply.github.com>
2021-03-03 09:18:59 +0000

    add optional dir change and start pyspark shell

--- HPC/myspark.sh
+++ HPC/myspark.sh
@@ -2,3 +2,6 @@ module load apps/java/jdk1.8.0_102/binary
 module load apps/python/conda
 
 source activate myspark
+
+# cd /data/*abc1de*/ScalableML  # *abc1de* should be replaced by your username
+# pyspark
